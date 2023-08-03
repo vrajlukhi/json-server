63d3bdca2d85fba0091dcd9ce83a598eb7722af7
vrajlukhi <130537393+vrajlukhi@users.noreply.github.com>
2023-08-03 11:06:26 +0530

    json server

--- index.js
+++ index.js
@@ -1,12 +1,81 @@
-fetch("db.json")
-    .then(res => res.json())
-    .then(json => document.getElementById("stud").addEventListener("click", student_data = () => {
-        for (i = 0; i <= 4; i++) {
-            console.log(json.student[i].name)
-            document.getElementById("output").innerHTML = `
-            <tr>
-            <td>${json.student[i].name}</td>
-            <td>${json.student[i].course}</td>
-        </tr>`
-        }
-}))
+const postList = document.getElementById("postList");
+
+function fetchPosts() {
+  fetch("http://localhost:3000/posts")
+    .then((response) => response.json())
+    .then((data) => {
+      renderPosts(data);
+    })
+    .catch((error) => {
+      console.error("Error fetching posts:", error);
+    });
+}
+
+function renderPosts(posts) {
+  postList.innerHTML = "";
+  posts.forEach((post) => {
+    const li = document.createElement("li");
+    li.innerHTML = `<strong> name :${post.name} <br> </strong> course : ${post.course} <br> </strong> gender : ${post.gender} <br> </strong>mobail : ${post.mobail}
+      <button onclick="updatePost(${post.id})">Edit</button>
+      <button onclick="deletePost(${post.id})">Delete</button>`;
+    postList.appendChild(li);
+  });
+}
+
+function createPost() {
+  const name = document.getElementById("name").value;
+  const course = document.getElementById("course").value;
+  const mobail = document.getElementById("mobail").value;
+  const gender = document.getElementById("gender").value;
+
+  fetch("http://localhost:3000/posts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, course, gender, mobail}),
+  })
+    .then(() => {
+      fetchPosts();
+    })
+    .catch((error) => {
+      console.error("Error creating post:", error);
+    });
+}
+
+function updatePost(id) {
+  const name = prompt("Enter new name:");
+  const course = prompt("Enter new course:");
+  const gender = prompt("Enter new gender:");
+  const mobail = prompt("Enter new mobail:");
+
+
+  fetch(`http://localhost:3000/posts/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, course, gender, mobail }),
+  })
+    .then(() => {
+      fetchPosts();
+    })
+    .catch((error) => {
+      console.error("Error updating post:", error);
+    });
+}
+
+function deletePost(id) {
+  fetch(`http://localhost:3000/posts/${id}`, {
+    method: "DELETE",
+  })
+    .then(() => {
+      fetchPosts();
+    })
+    .catch((error) => {
+      console.error("Error deleting post:", error);
+    });
+}
+
+// Fetch and render posts on page load
+fetchPosts();
